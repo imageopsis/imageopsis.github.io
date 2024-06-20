ce96449fb4e0cf89fe4d708b64d578b6137d6886
Dongpyo Hong <dhong@imageopsis.com>
2024-06-20 19:06:17 +0900

    [WIP] CSS styles and Responsive design

---
+++ scripts/imageopsis.js
@@ -0,0 +1,17 @@
+// // reading cookies
+// var allCookies = document.cookie
+// console.log(allCookies)
+
+// function getCookie(name) {
+//   let cookies = document.cookie.split('; ')
+  
+//   for (let i = 0; i < cookies.length; i++) {
+//     let cookie = cookies[i].split('=')
+//     if (cookie[0] == name) {
+//       return cookie[1]
+//     }
+//   }
+
+//   return null
+// }
+
